a30da77ea84bcd95b90a6b62cc327492f84bae86
Christophe Coenraets <ccoenraets@ccoenraets-ltm1.internal.salesforce.com>
2017-01-04 09:45:21 -0500

    code formatting

--- src/aura/VFWrapper/VFWrapperController.js
+++ src/aura/VFWrapper/VFWrapperController.js
@@ -1,27 +1,27 @@
 ({
     doInit : function(component, event, helper) {
-        var vfOrigin = component.get("v.vfOrigin");
+		var vfOrigin = component.get("v.vfOrigin");
 		window.addEventListener("message", function(event) {
-            if (event.origin !== vfOrigin) {
-   	            // Not the expected origin: reject message
+			if (event.origin !== vfOrigin) {
+				// Not the expected origin: reject message
 				return;
 			}
-            // Only handle messages we are interested in
-            if (event.data.name === "com.mycompany.chatmessage") {
-	            var vfMessages = component.get("v.vfMessages");
-    	        vfMessages = vfMessages + event.data.payload + "\n";
-        	    component.set("v.vfMessages", vfMessages);
-            }
+			// Only handle messages we are interested in
+			if (event.data.name === "com.mycompany.chatmessage") {
+				var vfMessages = component.get("v.vfMessages");
+				vfMessages = vfMessages + event.data.payload + "\n";
+				component.set("v.vfMessages", vfMessages);
+			}
 		}, false);
-    },
+	},
 
-    sendToVF : function(component, event, helper) {
-        var vfOrigin = component.get("v.vfOrigin");
-        var vfWindow = component.find("vfFrame").getElement().contentWindow;
-        var message = {
-            name: "com.mycompany.chatmessage",
-            payload: component.get("v.message")
-        };
+	sendToVF : function(component, event, helper) {
+		var vfOrigin = component.get("v.vfOrigin");
+		var vfWindow = component.find("vfFrame").getElement().contentWindow;
+		var message = {
+			name: "com.mycompany.chatmessage",
+			payload: component.get("v.message")
+		};
 		vfWindow.postMessage(message, vfOrigin);
 	}
 })
