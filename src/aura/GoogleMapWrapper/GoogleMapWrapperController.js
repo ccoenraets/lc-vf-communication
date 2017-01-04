5c0cbec87fc2717782295f04355985550ce2921a
Christophe Coenraets <ccoenraets@ccoenraets-ltm1.internal.salesforce.com>
2017-01-04 09:38:17 -0500

    code formatting

--- src/aura/GoogleMapWrapper/GoogleMapWrapperController.js
+++ src/aura/GoogleMapWrapper/GoogleMapWrapperController.js
@@ -1,17 +1,15 @@
 ({
   	recordChangeHandler : function(component, event) {
-        console.log("recordChangeHandler");
-        var id = event.getParam("recordId");
-        component.set("v.recordId", id);
-        var service = component.find("service");
-        service.reloadRecord();
+		var id = event.getParam("recordId");
+		component.set("v.recordId", id);
+		var service = component.find("service");
+		service.reloadRecord();
 	},
     
   	propertyChangeHandler : function(component, event) {
-        console.log("propertyChangeHandler");
-        var property = component.get("v.property");
-        var vfOrigin = component.get("v.vfOrigin");
-        var vfWindow = component.find("vfFrame").getElement().contentWindow;
+		var property = component.get("v.property");
+		var vfOrigin = component.get("v.vfOrigin");
+		var vfWindow = component.find("vfFrame").getElement().contentWindow;
 		vfWindow.postMessage(property, vfOrigin);
 	}
 })
